91691c82d05742a0994d8ee756d68a9209fcd448
daniela <danielacarlin@hotmail.com>
2017-04-10 21:33:54 -0500

    ultimo actualizado

---
+++ 2014222405/2014222405/Bus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2014222405
+{
+    public class Bus: Carro
+    {
+        public TipoBus TipoBus { get; set; }
+
+        public Bus(): base() {
+            TipoBus = TipoBus.NoDefinido;
+        }
+        public Bus(TipoCarro tipoCarro, TipoBus tipoBus):base()
+        {
+        TipoBus = tipoBus;
+        }
+        
+    }
+}
